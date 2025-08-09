e719d4b0eea6b400f1dfde4a7ac39f8a069ad2b5
Wiebe Vandendriessche <wiebe.vandendriessche@ugent.be>
2025-08-09 19:20:50 +0200

    setup i18n for the portfolio

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import {Link} from '@/i18n/navigation';
+import {useTranslations} from 'next-intl';
+
+export default function Home() {
+  const t = useTranslations('HomePage');
+  return (
+    <div>
+      <h1>{t('title')}</h1>
+      <Link href="/about">{t('about')}</Link>
+    </div>
+  );
+}
